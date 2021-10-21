b7cf871856f518b99b6b0bd8e2b751147e9dcc59
Julia <222596@edu.p.lodz.pl>
2021-10-21 12:59:44 +0200

    Metoda szukająca ilość wielkich liter

--- lab2/src/main/java/julia/pl/lab2/Controllers/Controller.java
+++ lab2/src/main/java/julia/pl/lab2/Controllers/Controller.java
@@ -1,10 +1,26 @@
 package julia.pl.lab2.Controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @GetMapping("/CapitalLetters")
+    public int findCapitalLetters(@RequestParam ("text") String text){
+        int counter = 0;
+        char sign;
+
+        for(int i = 0; i < text.length(); i++){
+            sign = text.charAt(i);
+            if( Character.isUpperCase(sign) )
+                counter++;
+        }
+
+        return counter;
+    }
 
 
 }
